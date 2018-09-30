e903ea201389df3072deda517edf11f34d0afb84
runfengai <runfengai@163.com>
2018-09-30 11:43:41 +0800

    查询功能检测

--- app/src/main/java/com/jarry/jarrydao/MainActivity.java
+++ app/src/main/java/com/jarry/jarrydao/MainActivity.java
@@ -3,10 +3,13 @@ package com.jarry.jarrydao;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.jarry.jarrydao.bean.Student;
 import com.jarry.jarrydaolib.db.BaseDao;
 import com.jarry.jarrydaolib.db.DBFactory;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,8 +24,25 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param view
      */
-    public void click(View view) {
+    public void insertClick(View view) {
         BaseDao<Student> baseDao = DBFactory.getInstance(this).getBaseDao(Student.class);
         baseDao.insert(new Student(1, "jarry", 18));
+    }
+
+    /**
+     * 查询操作
+     *
+     * @param view
+     */
+    public void query(View view) {
+        BaseDao<Student> baseDao = DBFactory.getInstance(this).getBaseDao(Student.class);
+        Student student = new Student();
+        List<Student> studentList = baseDao.query(student);
+        TextView viewById = (TextView) findViewById(R.id.text_res);
+        for (Student student1 : studentList) {
+            viewById.append(student1.id + " ");
+            viewById.append(student1.name + " ");
+            viewById.append(student1.age + " ");
+        }
     }
 }
--- app/src/main/java/com/jarry/jarrydao/bean/Student.java
+++ app/src/main/java/com/jarry/jarrydao/bean/Student.java
@@ -15,6 +15,9 @@ public class Student {
     @DBField("_age")
     public int age;
 
+    public Student() {
+    }
+
     public Student(int id, String name, int age) {
         this.id = id;
         this.name = name;
